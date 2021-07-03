528d628da98d391c13e73921955ee6e8fd0ccc8c
Aishwarya <75416297+Aishwarya-123-cloud@users.noreply.github.com>
2021-07-03 18:54:36 +0530

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,16 @@
+class Particle{
+    constrcutor(x,y){
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:false
+        }
+        this.r = 5;
+        this.body = Bodies.circle(x, y, this.r, options);
+        this.color = color(random(0,255),random(0,255),random(0,255));
+        World.add(world, this.body);
+    }
+    display(){
+        
+    }
+}
